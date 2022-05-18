ec82c0ec4800eb1cb88f4f8e77164fe4d0cc2b8b
Jorden Dickerson <jordendickerson24@gmail.com>
2022-05-18 14:45:36 -0600

    Exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let characters = string.length;
+    let index = string.length - 1;
+    let reversed = '';
+    for (let i=0; i < characters; i++){
+        let letter = string.charAt(index);
+        reversed += letter;
+        index -= 1;
+    }
+    return reversed;
 };
 
 // Do not edit below this line
