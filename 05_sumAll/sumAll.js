f9f14f2ce438b0258c5b1c5a6251eb68022c399e
Jorden Dickerson <jordendickerson24@gmail.com>
2022-05-20 10:28:06 -0600

    Exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    let sum = 0;
+    if (x < 0 || y < 0 || Number.isInteger(x) == false || Number.isInteger(y) == false){
+        return 'ERROR'
+    }
 
+    if (y>x){
+        for (let i=x; i<=y; i++){
+                sum += i;
+            }
+    }else if(x>y){
+        for (let i=y; i<=x; i++){
+            sum += i;
+        }
+    }
+    
+    return sum;
 };
 
 // Do not edit below this line
