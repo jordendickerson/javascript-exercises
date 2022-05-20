845bf74fa84973b41c75db5bce0dbe60462a8e54
Jorden Dickerson <jordendickerson24@gmail.com>
2022-05-20 10:39:06 -0600

    Exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,24 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (isDivisible(year, 4)){
+        if (isDivisible(year, 100)){
+            if (isDivisible(year, 400)){
+                return true;
+            }
+            return false;
+        }
+        return true;
+    }else{
+        return false;
+    }
 };
+
+const isDivisible = function(number, divider){
+    if (number % divider == 0){
+        return true;
+    }else{
+        return false;
+    }
+}
 
 // Do not edit below this line
 module.exports = leapYears;
