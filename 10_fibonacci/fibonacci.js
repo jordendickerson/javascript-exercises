2b535398a7e569cd317fcc141c8b01f399ee889b
Jorden Dickerson <jordendickerson24@gmail.com>
2022-06-04 12:04:20 -0600

    Exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(place) {
+    let x = 1;
+    let y = 1;
+    if (parseInt(place) < 0){
+        return "OOPS";
+    }
+    for (let i=2; i<parseInt(place); i++){
+        let xValue = x;
+        x=y;
+        y+=xValue;
+    }
+    return y;
 };
 
 // Do not edit below this line
