e4362932be5a58c19bd5741188f2b12c7509d5e7
Jorden Dickerson <jordendickerson24@gmail.com>
2022-06-04 11:17:29 -0600

    Exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
+const palindromes = function (palindrome) {
+    let fixed = palindrome.toLowerCase();
+    let toRemove = [" ", ",", ".", "!", "?"];
+    for (let i=0; i<5; i++){
+        fixed = fixed.replaceAll(toRemove[i], '');
+    }
+    return fixed === reverseString(fixed) ? true: false;
+};
 
+const reverseString = function(string) {
+    let characters = string.length;
+    let index = string.length - 1;
+    let reversed = '';
+    for (let i=0; i < characters; i++){
+        let letter = string.charAt(index);
+        reversed += letter;
+        index -= 1;
+    }
+    return reversed;
 };
 
 // Do not edit below this line
