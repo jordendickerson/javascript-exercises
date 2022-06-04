fd00105673ff35684ec15fd7476194f7a151fad5
Jorden Dickerson <jordendickerson24@gmail.com>
2022-06-04 12:30:22 -0600

    Exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let bookArray = [];
+    books.map(book => bookArray.push(book.title));
+    return bookArray;
 };
 
 // Do not edit below this line
