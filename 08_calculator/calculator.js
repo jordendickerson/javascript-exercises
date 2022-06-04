56796369633c1a49cab9ddc65296c6d7a4e9b1b3
Jorden Dickerson <jordendickerson24@gmail.com>
2022-06-04 10:53:55 -0600

    Exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(x,y) {
+	return x+y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x,y) {
+	return x-y;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  let sum = 0;
+  for (let i=0; i < numbers.length; i++){
+    sum += numbers[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  let product = 1;
+  for (let i=0; i < numbers.length; i++){
+    product *= numbers[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(x, powerOf) {
+	return x**powerOf;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  let factorial = 1;
+	for (let i=1; i<=x; i++){
+    factorial *= i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
