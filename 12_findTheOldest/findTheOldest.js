85d91045194222abd40d35783d1e52a9a94037e6
Jorden Dickerson <jordendickerson24@gmail.com>
2022-06-04 13:02:40 -0600

    Exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const oldToYoung = people.sort(function(a, b){
+        if (!a.hasOwnProperty("yearOfDeath")){
+            a.yearOfDeath = (new Date()).getFullYear()
+        }
+        if (!b.hasOwnProperty("yearOfDeath")){
+            b.yearOfDeath = (new Date()).getFullYear()
+        }
 
+        if((a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth)) {
+        return -1;
+        } else {
+        return 1;
+        };
+    });
+    return oldToYoung[0];
 };
 
 // Do not edit below this line
